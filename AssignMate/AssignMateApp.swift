e7f6d3f97ce9ec629767935c9051758407b7b670
Jesse <jessechhina@icloud>
2023-10-13 10:47:10 -0400

    Initial Commit

---
+++ AssignMate/AssignMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignMateApp.swift
+//  AssignMate
+//
+//  Created by Jesse Chhina on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct AssignMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AssignMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AssignMate
+//
+//  Created by Jesse Chhina on 10/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
